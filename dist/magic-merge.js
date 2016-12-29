2d58f5263556f89b8d8a1a9bc672561b89ce2715
Misha Koryak <mkoryak@gmail.com>
2016-12-28 23:47:39 -0500

    wat you doing here?

--- dist/magic-merge.js
+++ dist/magic-merge.js
@@ -16,7 +16,7 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 
 function _asyncToGenerator(fn) { return function () { var gen = fn.apply(this, arguments); return new Promise(function (resolve, reject) { function step(key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { return Promise.resolve(value).then(function (value) { step("next", value); }, function (err) { step("throw", err); }); } } return step("next"); }); }; }
 
-class MagicMerge extends _events2.default {
+exports.default = class extends _events2.default {
 
     // settings.org         string, org name - catalant
     // settings.interval    number, interval in ms how often to re-check for prs
@@ -185,5 +185,5 @@ class MagicMerge extends _events2.default {
         })();
     }
 
-}exports.default = MagicMerge;
+};
 ;
