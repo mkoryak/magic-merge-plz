2ca8dcc601df21a369e87b5fb5937b3c6554f44e
Misha Koryak <mkoryak@users.noreply.github.com>
2017-01-01 18:37:08 -0500

    Update cats.js

--- src/cats.js
+++ src/cats.js
@@ -1,15 +1,1 @@
-import needle from 'needle';
-
-const url = 'http://thecatapi.com/api/images/get?format=html&results_per_page=1&size=full&api_key=MTM1MTQ;';
-
-export default () => {
-    return new Promise((resolve, reject) => {
-        needle.get(url, (err, response, body) => {
-            if (body && body.match(/src="([^"]+)/)) {
-                resolve(RegExp.$1);
-            } else {
-                reject();
-            }
-        });
-    });
-};
+adsfasdf
