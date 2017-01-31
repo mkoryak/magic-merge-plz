3137a508af1e7496070ffcaa71ede185f6b2d11b
Misha Koryak <mkoryak@gmail.com>
2017-01-31 14:25:24 -0500

    wooo

--- src/magic-merge.js
+++ src/magic-merge.js
@@ -259,15 +259,6 @@ export default class extends EventEmitter {
             this.emit('debug', `[${repo}] has ${prs.length} open PRs`);
 
             prs.forEach(async pr => {
-
-
-                if (ticket) {
-                    const branchName = pr.head.label.split(':')[1];
-                    const branchPreviewMsg = `[:mag: open in branch preview](unreal:${branchName})    [link not working?](https://wiki.gocatalant.com/wiki/Branch_preview_tool#Installing_branch_preview_url_handler)`;
-
-                    const msg = await this.jira.getTicketSummary(ticket);
-                    this.addConditionalComment(queue, 'hooray', [msg, branchPreviewMsg].join('\n'));
-                }
                 
                 const queue = this.makeQueue(repo, pr);
                 this.addInfoComment(queue);
